d102a46a798466368280e0feb54791c83cbb93e5
ALEXANDER DITZEND <alexander@ALEXANDERs-iMac.local>
2018-03-27 18:54:10 -0300

    no sql

--- imports/startup/server/connection.js
+++ imports/startup/server/connection.js
@@ -1,24 +1,24 @@
-import { Meteor } from 'meteor/meteor';
-import mysql from 'mysql';
+// import { Meteor } from 'meteor/meteor';
+// import mysql from 'mysql';
 
-const conn = function (offset) {
-    console.log("connecting to sql db");
-    const promise = new Promise(function (resolve, reject) {
-        const connection = mysql.createConnection({
-            host: '98.130.0.77',
-            user: 'pross88_gtuser',
-            password: Meteor.settings.IX_DB_PASS,
-            database: 'pross88_gtsystem',
-            insecureAuth: "true"
-        });
-        connection.connect(function (err) {
-            if (err) {
-                reject(err.stack);
-            } else {
-                resolve(connection);
-            }
-        });
-    });//connect
-    return promise;
-};
-export { conn };
+// const conn = function (offset) {
+//     console.log("connecting to sql db");
+//     const promise = new Promise(function (resolve, reject) {
+//         const connection = mysql.createConnection({
+//             host: '98.130.0.77',
+//             user: 'pross88_gtuser',
+//             password: Meteor.settings.IX_DB_PASS,
+//             database: 'pross88_gtsystem',
+//             insecureAuth: "true"
+//         });
+//         connection.connect(function (err) {
+//             if (err) {
+//                 reject(err.stack);
+//             } else {
+//                 resolve(connection);
+//             }
+//         });
+//     });//connect
+//     return promise;
+// };
+// export { conn };
