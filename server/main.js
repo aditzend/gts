8e7066597601866268b2a4c8f341d5b3731441ca
Alexander Ditzend <alexander.ditzend@mitrol.net>
2018-07-12 19:56:19 -0300

    mails andando perfecto

--- server/main.js
+++ server/main.js
@@ -10,7 +10,8 @@ import {
 if (Meteor.isServer) {
     Meteor.startup(() => {
         process.env.MAIL_URL = Meteor.settings.mailgun.url;
-        Meteor.call("checkEmailJobs");
+        Meteor.setInterval(() => Meteor.call("checkEmailJobs"), 2 * 1000);
+        // Meteor.setInterval(() =>console.log('hola alex'), 1000);
         
     });
 }
